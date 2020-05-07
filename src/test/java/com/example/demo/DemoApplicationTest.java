a647fb678790aa30adf30b17bdd81209ab8bf2ac
Moses Niyonshuti <mniyonshuti@Mosess-MBP.attlocal.net>
2020-05-06 22:41:33 -0500

    test

---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoApplicationTest {
+
+    DemoApplication demoApplication;
+
+    @BeforeEach
+    void setUp() {
+        demoApplication = new DemoApplication();
+    }
+
+    @Test
+    void add() {
+        assertEquals(demoApplication.add(2,2), 4);
+    }
+}
